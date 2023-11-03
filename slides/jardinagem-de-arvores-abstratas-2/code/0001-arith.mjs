00d9586555f5af217e1156eb17c2c64ff8a6039a
Lincoln Clarete <lincoln@clarete.li>
2023-11-03 17:49:45 -0300

    add comment about stack

--- slides/jardinagem-de-arvores-abstratas-2/code/0001-arith.mjs
+++ slides/jardinagem-de-arvores-abstratas-2/code/0001-arith.mjs
@@ -2,20 +2,20 @@ import { Instruction, interpreter } from "./bytecode.mjs"
 
 // 1 + 2 * 3 = 7
 console.log(interpreter([
-  Instruction.PUSH, 3,
-  Instruction.PUSH, 2,
-  Instruction.MUL,
-  Instruction.PUSH, 1,
-  Instruction.SUM,
+  Instruction.PUSH, 3,    // stack: [3]
+  Instruction.PUSH, 2,    // stack: [3, 2]
+  Instruction.MUL,        // stack: [6]
+  Instruction.PUSH, 1,    // stack: [6, 1]
+  Instruction.SUM,        // stack: [7]
   Instruction.HALT,
 ]));
 
 // (1 + 2) * 3 = 9
 console.log(interpreter([
-  Instruction.PUSH, 2,
-  Instruction.PUSH, 1,
-  Instruction.SUM,
-  Instruction.PUSH, 3,
-  Instruction.MUL,
+  Instruction.PUSH, 2,    // stack: [2]
+  Instruction.PUSH, 1,    // stack: [2, 1]
+  Instruction.SUM,        // stack: [3]
+  Instruction.PUSH, 3,    // stack: [3, 3]
+  Instruction.MUL,        // stack: [9]
   Instruction.HALT,
 ]));
